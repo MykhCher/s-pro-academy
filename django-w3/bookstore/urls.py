3598e795de84d4ae313fa1cf9ec00068f31ebff4
MykhCher <106117538+MykhCher@users.noreply.github.com>
2022-07-15 14:45:15 +0300

    Delete django-w3 directory

--- django-w3/bookstore/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import author_detail, book_detail, book_list, book_authlist
-
-urlpatterns = [
-    path('', book_list, name='book_list'),
-    path('book/<int:book_id>', book_detail, name='book_detail'),
-    path('auth/<int:auth_id>', author_detail, name='auth_detail'),
-    path('book/by_auth/<int:auth_id>', book_authlist, name='book_authlist'),
-]
